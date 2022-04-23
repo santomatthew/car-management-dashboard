84f6d3b1360d32e45d6623b6a2287507e240ed1c
santo matthew <matthewalesandro78@gmail.com>
2022-04-24 01:06:48 +0700

    add feature delete and redirection for every action

--- controller/car-delete.js
+++ controller/car-delete.js
@@ -2,20 +2,33 @@ const { Cars } = require("../models");
 
 async function carDelete(req, res) {
   let idFromParams = req.params.id;
+
+  let arrayId = [];
+  let splitId = idFromParams.split("");
+
+  for (let i in splitId) {
+    if (splitId[i] != ":") {
+      arrayId.push(splitId[i]);
+    }
+  }
+  let finalId = arrayId.join("");
+  console.log(finalId);
+
   let car = await Cars.findAll();
 
   let array = [];
   for (let i in car) {
-    if (idFromParams == car[i].id) {
+    if (finalId == car[i].id) {
       array.push(car[i].id);
     }
   }
 
   if (array.length == 0) {
-    res.json(`Data ${idFromParams} tidak ada`);
+    console.log(`Data ${finalId} tidak ada`);
+    res.json(`Data ${finalId} tidak ada`);
   } else {
-    await Cars.destroy({ where: { id: idFromParams } });
-    res.json(`Data ${req.params.id} berhasil di hapus`);
+    await Cars.destroy({ where: { id: finalId } });
+    res.json(`Data ${finalId} berhasil di hapus`);
   }
 }
 
--- controller/form.js
+++ controller/form.js
@@ -1,11 +1,32 @@
-function form(req, res) {
+const { Cars } = require("../models");
+async function form(req, res) {
   let id = req.params.id;
-  let title = "Add Car";
+  let decider = false;
+  let realcar;
   if (id) {
-    title = "Update Car Information";
+    let car = await Cars.findOne({ where: { id: id } });
+    realcar = car;
+    decider = true;
+  } else {
+    decider = false;
   }
 
-  res.status(200).render("form", { title: title, id: id });
+  let title = "Add Car";
+  let name, price, size_id;
+  if (decider) {
+    title = "Update Car Information";
+    name = realcar.name;
+    price = realcar.price;
+    size_id = realcar.size_id;
+  }
+
+  res.status(200).render("form", {
+    title: title,
+    id: id,
+    name: name,
+    price: price,
+    size_id: size_id,
+  });
 }
 
 module.exports = form;
